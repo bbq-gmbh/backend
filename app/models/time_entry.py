7dd51c69f08c013c6e3a101efb997b308cf324ae
c90x <e.0xcc3@gmail.com>
2025-10-17 15:29:28 +0200

    Fix foreign key reference for user_id in TimeEntry model

--- app/models/time_entry.py
+++ app/models/time_entry.py
@@ -14,7 +14,7 @@ class TimeEntryType(Enum):
 class TimeEntry(SQLModel, table=True):
     __tablename__: str = "time_entries"
     id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: str = Field(foreign_key="users.id", index=True)
+    user_id: str = Field(foreign_key="employees.id", index=True)
     author_id: str = Field(foreign_key="users.id", index=True)
     entry_type: TimeEntryType
     date_time: datetime = Field(index=True)
