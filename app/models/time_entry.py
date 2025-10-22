5803aededf36d5fdac3bee91059e18e93da859fc
c90x <e.0xcc3@gmail.com>
2025-10-22 15:58:14 +0200

    Refactor TimeEntry model and schemas to include created_by and updated_by fields for better tracking of entries

--- app/models/time_entry.py
+++ app/models/time_entry.py
@@ -15,12 +15,13 @@ class TimeEntry(SQLModel, table=True):
     __tablename__: str = "time_entries"
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: uuid.UUID = Field(foreign_key="employees.user_id", index=True)
-    author_id: uuid.UUID = Field(foreign_key="users.id", index=True)
     entry_type: TimeEntryType
     date_time: datetime = Field(index=True)
+    created_by: uuid.UUID = Field(foreign_key="users.id", index=True)
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc), index=True
     )
+    updated_by: Optional[uuid.UUID] = Field(foreign_key="users.id", index=True)
     last_updated: datetime = Field(
         default_factory=lambda: datetime.now(timezone.utc),
         sa_column_kwargs={"onupdate": datetime.now(timezone.utc)},
--- app/schemas/time_entry.py
+++ app/schemas/time_entry.py
@@ -1,9 +1,21 @@
+from datetime import datetime
 import uuid
 
 from pydantic import BaseModel
 
+from app.models.time_entry import TimeEntryType
+
 
 class TimeEntryCreate(BaseModel):
     user_id: uuid.UUID
-    # TODO
+    entry_type: TimeEntryType
+    date_time: datetime
 
+
+class TimeEntryUpdate(BaseModel):
+    id: int
+    date_time: datetime
+
+
+class TimeEntryDelete(BaseModel):
+    id: int
