e6dfcd0acc270b978891ab08d0e8d28d2b863ee3
c90x <e.0xcc3@gmail.com>
2025-11-04 20:02:56 +0100

    Add EMPLOYEE_TIME_ENTRY_EDIT_MAX_DAYS setting for time entry edit duration

--- app/config/settings.py
+++ app/config/settings.py
@@ -32,6 +32,7 @@ class Settings:
     REFRESH_TOKEN_EXPIRE_DAYS: int = int(_get_env("REFRESH_TOKEN_EXPIRE_DAYS", "7"))
 
     EMPLOYEE_MAX_HIRARCHY_LEVELS: int = 20
+    EMPLOYEE_TIME_ENTRY_EDIT_MAX_DAYS: int = 7 * 1 # 1 week
 
 
 settings = Settings()
